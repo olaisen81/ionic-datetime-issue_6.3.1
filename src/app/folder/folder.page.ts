1201c7a95012a77599ed43da35db355fe5ce10ee
Fabio Perfetti <fabio.perfetti@passepartout.sm>
2022-10-13 09:57:17 +0200

    .

--- src/app/folder/folder.page.ts
+++ src/app/folder/folder.page.ts
@@ -1,5 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+/* eslint-disable @typescript-eslint/member-ordering */
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
+import { IonDatetime } from '@ionic/angular';
+import { format, parseISO } from 'date-fns';
 
 @Component({
   selector: 'app-folder',
@@ -7,12 +11,61 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./folder.page.scss'],
 })
 export class FolderPage implements OnInit {
+  @ViewChild('ionDaData') ionDaData: IonDatetime;
   public folder: string;
+  public form: FormGroup;
+  public sDaDataNoBind: string;
+  public get daData() { return this.form.get('daData'); }
+  public get daDataNoBind() { return this.form.get('daDataNoBind'); }
 
   constructor(private activatedRoute: ActivatedRoute) { }
 
   ngOnInit() {
     this.folder = this.activatedRoute.snapshot.paramMap.get('id');
+
+    this.form = new FormGroup({});
+    this.form.addControl('daData', new FormControl('', []));
+    this.form.addControl('daDataNoBind', new FormControl('', []));
+  }
+
+  /**
+   * Evento che scatta quando una nuova data è selezionata
+   */
+   public daDataChanged() {
+
+    if (this.ionDaData) {
+      //Setto la data per il formato corretto su DB
+      const date = format(parseISO(this.ionDaData.value as string), 'yyyy-MM-dd', { locale: {code: 'it'} });
+      this.form.controls.daData.setValue(date);
+
+      //Setto la data in formato corretto per essere visualizzata (es. 22 giugno 1981)
+      const sData = format(parseISO(this.ionDaData.value as string), 'dd MMMM yyyy', { locale: {code: 'it'} });
+      this.form.controls.daDataNoBind.setValue(sData);
+    }
+  }
+
+  /**
+   * Annulla la data
+   */
+   public clearDaData() {
+    this.ionDaData.reset();
+    this.ionDaData.cancel(true);
+    this.form.controls.daData.setValue(null);
+    this.form.controls.daDataNoBind.setValue(null);
+  }
+
+  /**
+   * Chiude la dialog senza fare nulla
+   */
+  public closeDaData() {
+    this.ionDaData.cancel(true);
+  }
+
+  /**
+   * Conferma la data
+   */
+   public confirmDaData() {
+    this.ionDaData.confirm(true);
   }
 
 }
